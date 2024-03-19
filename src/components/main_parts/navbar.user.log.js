746bd7f6f23b00569fb0ddc0bef4d0ec9e914740
lahir <lahir@NITRO5>
2024-03-19 20:44:56 +0530

    add

--- src/components/main_parts/navbar.user.log.js
+++ src/components/main_parts/navbar.user.log.js
@@ -81,6 +81,11 @@ function NavBar() {
                             <MDBNavbarLink href='EventBudget' tabIndex={-1} aria-disabled='true' style={{ color: '#DCDCDC', cursor: 'pointer' }}>
                                 Events Budget
                             </MDBNavbarLink>
+                        </MDBNavbarItem> 
+                        <MDBNavbarItem>
+                            <MDBNavbarLink href='Feedback' tabIndex={-1} aria-disabled='true' style={{ color: '#DCDCDC', cursor: 'pointer' }}>
+                               FeedBack
+                            </MDBNavbarLink>
                         </MDBNavbarItem>
                     </MDBNavbarNav>
 
