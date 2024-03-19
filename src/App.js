ba5666cab5b3822233cd0a628bf623d659f38839
lahir <lahir@NITRO5>
2024-03-19 20:14:45 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import Index from './components/index';
 import UserLogin from './components/user.login';
 import UserRegistration from './components/user.registration';
 import UserProfile from './components/user.profile';
-
 import About from './components/user.About';
 import Admin from './components/Admin/admin';
 import AdminLogin from './components/Admin/admin.login';
 import AdminReg from './components/Admin/admin.reg';
-
-
-import { BrowserRouter as Router, Route } from 'react-router-dom'
 import RequestEvent from './components/userAdmin/requestEvent';
-import AppointmentEdit from './components/userAdmin/appoimentEdit';
-import HaircareDashboard from './components/Admin/haircareDashboard';
-import Haircare from './components/userAdmin/haircare';
-import Nailpedicare from './components/userAdmin/nailpedicare';
 import EventBudget from './components/userAdmin/eventBudget';
-import NailpedicareDashboard from './components/Admin/nailpedicareDashboard';
-import DressDashboard from './components/Admin/dressDashboard';
-import Dress from './components/userAdmin/dress';
 import Payment from './components/userAdmin/payment';
 import Paymentdashboard from './components/Admin/paymentdashboard';
 import UserAdminDashboard from './components/userAdmin/userAdminDashboard';
-import ProductDashboard from './components/Admin/productDashboard';
-import Product from './components/userAdmin/product';
-import PacakageDashboard from './components/Admin/pacakageDashboard';
-import Package from './components/userAdmin/package';
-import Ownpackage from './components/userAdmin/ownpackage';
-import LeaveDashboard from './components/Admin/leaveDashboard';
-import AllleavesDashboard from './components/Admin/allleavesDashboard';
 import UserDashboard from './components/Admin/userDashboard';
 import ReqEventDashboard from './components/Admin/reqEventDashboard';
 import FinanceDashboard from './components/Admin/finanaceManagement';
@@ -46,23 +29,9 @@ function App() {
         <Route path="/EventBudget" exact component={EventBudget} />
         <Route path="/FinanceDashboard" exact component={FinanceDashboard} />
         <Route path="/ReqEventDashboard" exact component={ReqEventDashboard} />
-        <Route path="/ProductDashboard" exact component={ProductDashboard} />
-        <Route path="/AppointmentEdit" exact component={AppointmentEdit} />
-        <Route path="/HaircareDashboard" exact component={HaircareDashboard} />
-        <Route path="/Haircare" exact component={Haircare} />
-        <Route path="/Nailpedicare" exact component={Nailpedicare} />
-        <Route path="/NailpedicareDashboard" exact component={NailpedicareDashboard} />
-        <Route path="/DressDashboard" exact component={DressDashboard} />
-        <Route path="/Dress" exact component={Dress} />
         <Route path="/Payment" exact component={Payment} />
         <Route path="/Paymentdashboard" exact component={Paymentdashboard} />
-        <Route path="/Product" exact component={Product} />
-        <Route path="/PacakageDashboard" exact component={PacakageDashboard} />
-        <Route path="/Package" exact component={Package} />
-        <Route path="/Ownpackage" exact component={Ownpackage} />
-        <Route path="/LeaveDashboard" exact component={LeaveDashboard} />
         <Route path="/UserDashboard" exact component={UserDashboard} />
-        <Route path="/AllleavesDashboard" exact component={AllleavesDashboard} />
         <Route path="/UserAdminDashboard" exact component={UserAdminDashboard} />
         <Route path="/UserLogin" exact component={UserLogin} />
         <Route path="/UserRegistration" exact component={UserRegistration} />
